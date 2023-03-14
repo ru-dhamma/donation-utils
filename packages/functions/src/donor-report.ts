f43048145210b265d4b28ed6b084aa32fb2b7601
Petr Reshetin <preshetin@gmail.com>
2023-03-14 19:08:19 +0500

    some progress

--- packages/functions/src/donor-report.ts
+++ packages/functions/src/donor-report.ts
@@ -1,5 +1,6 @@
 import { ApiHandler } from "sst/node/api";
 import { Time } from "@urd/core/time";
+import {Pdf} from '@urd/core/pdf'
 import { connection } from "@urd/core/db";
 
 type DonationWithUserDataRow = {
@@ -19,15 +20,14 @@ export const handler = ApiHandler(async (_evt) => {
   // build html
   // (optionally) build pdf from html
 
-//  const donationsQuery = `select * from donations left join user on user.id = donations.user_id where created_at BETWEEN (CURDATE() - INTERVAL 30 DAY) AND CURDATE();`
-
+  //  const donationsQuery = `select * from donations left join user on user.id = donations.user_id where created_at BETWEEN (CURDATE() - INTERVAL 30 DAY) AND CURDATE();`
 
   // Stolen from https://stackoverflow.com/a/2090235/4990125
   const donationsQuery = `select * from donations left join user on user.id = donations.user_id 
     WHERE status = 'paid'
     AND
     YEAR(created_at) = YEAR(CURRENT_DATE - INTERVAL 2 MONTH)
-    AND MONTH(created_at) = MONTH(CURRENT_DATE - INTERVAL 2 MONTH)`
+    AND MONTH(created_at) = MONTH(CURRENT_DATE - INTERVAL 2 MONTH)`;
 
   const [rows] = await connection().query(donationsQuery);
 
@@ -39,46 +39,80 @@ export const handler = ApiHandler(async (_evt) => {
   //  { id: 2001, payment_instruction_id: 2001, recurrent_donation_id: null, user_id: 2001, status: 'paid', amount: '1.00000', is_rebilling: 1, is_automatic: 0,
   //    purpose: 'statute_activity', created_at: "2018-03-10T13:53:28.000Z", email: 'preshetin+prodMonthly@gmail.com', platform: 'dhamma-dullabha.org' },
   //  ];
- 
-  
-//  console.log('row', rows)
 
+  //  console.log('row', rows)
 
   const purposesList = [...new Set(donRows.map((el) => el.purpose))];
 
   console.log("purposesList", purposesList);
 
-  let html = "";
-  for (const purpose of purposesList) {
-    const oneTimeDonations = donRows.filter((don) => don.purpose === purpose && don.is_automatic === 0);
-    const recurrentDonations = donRows.filter((don) => don.purpose === purpose && don.is_automatic === 1);
+  let html = `
+  <h1>Online Donations</h1>
+  <p>Period: January 2023</p>
+  
+  `;
 
-    console.log('recurrentDonations', recurrentDonations.length)
+  for (const purpose of purposesList) {
+    const oneTimeDonations = donRows.filter(
+      (don) => don.purpose === purpose && don.is_automatic === 0
+    );
+    const recurrentDonations = donRows.filter(
+      (don) => don.purpose === purpose && don.is_automatic === 1
+    );
+
+    console.log("recurrentDonations", recurrentDonations.length);
 
     html += `
-  <h1>${purpose}</h1>
+  <hr />
+  <h2>${capitalizeFirstLetter(purpose.replace(/_/g," "))}</h2>
 <div class="row">
   <div class="column">
 
   
-  <h2>One-time donations</h2>
+  <h3>One-Time Donations</h3>
   ${buildDonorsHtmlForPurpose(purpose, oneTimeDonations)}
 
   </div>
   <div class="column">
   
-  <h2>Recurrent donations</h2>
+  <h3>Recurrent Donations</h3>
   ${buildDonorsHtmlForPurpose(purpose, recurrentDonations)}
   
   </div>
 </div>
-    `
+    `;
   }
+
+  html = wrapHtml(html)
+
+  // const reportPdf = (await Pdf.create(html)) as Buffer;
+  // let response = {
+  //   statusCode: 200,
+  //   headers: {
+  //     "Content-type": "application/pdf", //you can change any content type
+  //     "content-disposition": "attachment; filename=report-Jan-2023.pdf", // key of success
+  //   },
+  //   body: reportPdf.toString("base64"),
+  //   isBase64Encoded: true,
+  // };
+  // return response;
+
+
+  // const pdf = (await Pdf.create(html)).data;
 
   return {
     statusCode: 200,
+
     headers: { "content-type": "text/html" },
-    body: wrapHtml(html),
+    body: html
+
+      //  headers: {
+      //     "Content-Type": "application/pdf", //you can change any content type
+      //     "Content-Disposition": "inline; filename=report-Jan-2023.pdf", // key of success
+      //   },
+
+    // headers: { "content-type": "application/json" },
+    // body: JSON.stringify(pdf.data)
   };
 });
 
@@ -95,7 +129,13 @@ function buildDonorsHtmlForPurpose(
     <th>Amount</th>
   </tr>
     ${donations
-      .map((don) => `<tr> <td>${don.created_at.toLocaleDateString()}</td>  <td> ${don.email}</td>  <td>${parseInt(don.amount)}₽</td> </tr>`).join('')}
+      .map(
+        (don) =>
+          `<tr> <td>${don.created_at.toLocaleDateString()}</td>  <td> ${
+            don.email
+          }</td>  <td style="text-align: right">${numberWithCommas(parseInt(don.amount))}&nbsp;&#8381</td> </tr>`
+      )
+      .join("")}
   </table>
   `;
   return result;
@@ -142,4 +182,19 @@ tr {
 </body>
 </html>
   `;
+}
+
+
+function capitalizeFirstLetter(str: string) {
+  const arr = str.split(" ");
+
+  for (var i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+
+  return arr.join(" ")
+}
+
+function numberWithCommas(x: number) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
