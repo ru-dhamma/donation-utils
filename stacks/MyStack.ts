86e272afc36add8f9f2d528c4f2a309a29b9deea
Petr Reshetin <preshetin@gmail.com>
2023-03-21 17:25:54 +0500

    some state

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,13 +1,34 @@
-import { StackContext, Api } from "sst/constructs";
+import { StackContext, Api, Function } from "sst/constructs";
+import { LayerVersion } from "aws-cdk-lib/aws-lambda";
+
+const layerArn =
+  "arn:aws:lambda:us-east-1:764866452798:layer:chrome-aws-lambda:25";
 
 export function API({ stack }: StackContext) {
+  const layer = LayerVersion.fromLayerVersionArn(stack, "Layer", layerArn);
+  
   const api = new Api(stack, "api", {
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
       "POST /unsubscribe": "packages/functions/src/unsubscribe.handler",
-      "GET /donor-report": "packages/functions/src/donor-report.handler",
+      "GET /donor-report": {
+        function: {
+          runtime: "nodejs14.x",
+          timeout: 15,
+          handler: "packages/functions/src/donor-report.handler",
+          nodejs: {
+            esbuild: {
+              external: ["chrome-aws-lambda"],
+            },
+          },
+          layers: [ layer ],
+        },
+      },
+
+      //  "GET /donor-report": "packages/functions/src/donor-report.handler",
     },
   });
+
   stack.addOutputs({
     ApiEndpoint: api.url,
   });
