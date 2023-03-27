1731104691a54a7fa9ed95fd01218b3171415e8e
Petr Reshetin <preshetin@gmail.com>
2023-03-27 17:20:03 +0500

    increase timeout for report handler

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,10 +1,9 @@
 import { StackContext, Api, Queue, Config, Function } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
- 
   const SLACK_BOT_TOKEN = new Config.Secret(stack, "SLACK_BOT_TOKEN");
   const SLACK_SIGNING_SECRET = new Config.Secret(stack, "SLACK_SIGNING_SECRET");
-  
+
   const api = new Api(stack, "api", {
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
@@ -25,13 +24,27 @@ export function API({ stack }: StackContext) {
   });
   checkOrSyncDbQueue.bind([SLACK_BOT_TOKEN]);
 
-  const handleDonationsReportQueue = new Queue(stack, "HandleDonationsReportQueue", {
-    consumer: "packages/functions/src/handleDonationsReport.main",
-  });
+  const handleDonationsReportQueue = new Queue(
+    stack,
+    "HandleDonationsReportQueue",
+    {
+      consumer: {
+        function: {
+          timeout: 30,
+          handler: "packages/functions/src/handleDonationsReport.main",
+        },
+      },
+    }
+  );
   handleDonationsReportQueue.bind([SLACK_BOT_TOKEN]);
 
-  api.bind([checkOrSyncDbQueue, handleDonationsReportQueue, SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET]);
-  
+  api.bind([
+    checkOrSyncDbQueue,
+    handleDonationsReportQueue,
+    SLACK_BOT_TOKEN,
+    SLACK_SIGNING_SECRET,
+  ]);
+
   stack.addOutputs({
     ApiEndpoint: api.url,
   });
