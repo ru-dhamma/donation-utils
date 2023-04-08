c90cca3a40d75aebd0519a035bf9b93a7223a200
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2023-04-08 16:52:53 +0300

    Filename format for donation reports is updated to have it simpler, but with support for monthly and yearly reports

--- packages/functions/src/handleDonationsReport.ts
+++ packages/functions/src/handleDonationsReport.ts
@@ -56,8 +56,10 @@ async function handleMessage(event: DonationReportEvent) {
 
   const pdf = await downloadFile(pdfLink);
 
+  const period = from.month === to.month ? from.toFormat('yyyy-MM') : from.toFormat('yyyy');
+
   const res = await slackClient(Config.SLACK_BOT_TOKEN).files.upload({
-    filename: `donations-report-from-${from.toFormat('yyyy-MM-dd')}-to-${to.toFormat('yyyy-MM-dd')}.pdf`,
+    filename: `donations_${period}.pdf`,
     initial_comment: "Here is the report.",
     title: "Online Donations at Dhamma Dullabha",
     filetype: "pdf",
@@ -70,7 +72,7 @@ async function handleMessage(event: DonationReportEvent) {
   const resBookkeeperCsv = await slackClient(
     Config.SLACK_BOT_TOKEN
   ).files.upload({
-    filename: `bookkeeper-report-from-${from.toFormat('yyyy-MM-dd')}-to-${to.toFormat('yyyy-MM-dd')}.csv`,
+    filename: `donations_${period}.csv`,
     initial_comment: "Here is CSV file for bookkeeper.",
     title: "Donations List for Bookkeeper",
     filetype: "csv",
